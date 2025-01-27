1d52dc3f963b2994a5df402333aa961d2b87310d
krasi <krasimir.xristov@gmail.com>
2025-01-27 18:18:27 +0100

    create a create-story route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@heroui/button';
 import Header from './_components/Header';
+import Hero from './_components/Hero';
 
 export default function Home() {
   return (
     <div className='bg-[#cad3ff]'>
       <Header />
 
-      {/* Hero */}
+      <Hero />
     </div>
   );
 }
