a340489581933145a0b7d0fc5c1e9151a7041e3d
krasi <krasimir.xristov@gmail.com>
2025-01-27 16:02:11 +0100

    recreate the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,18 @@
-import { heroui } from "@heroui/react";
 import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [heroui({
-    themes:{
-      light:{
-        //...
-        colors: {},
-      },
-      dark:{
-        //...
-        colors: {},
-      }
-      // ... custom themes
-    },
-  })],
-} satisfies Config
+  plugins: [],
+} satisfies Config;
