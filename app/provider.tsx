a8f3e188d32862a399b6df98a6eb684da22f1dfe
krasi <krasimir.xristov@gmail.com>
2025-01-30 20:25:26 +0100

    install clerk and test protective routes

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { ClerkProvider } from '@clerk/nextjs';
 import { HeroUIProvider } from '@heroui/react';
 import React from 'react';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <ClerkProvider>
+      <HeroUIProvider>{children}</HeroUIProvider>;
+    </ClerkProvider>
+  );
 };
 
 export default Provider;
