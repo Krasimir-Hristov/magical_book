f4b1dad69c397993a1bc9306cc39dea66b278fca
krasi <krasimir.xristov@gmail.com>
2025-02-01 12:44:17 +0100

    implement the input component StorySubjdectInput.tsx

--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+'use client';
 
+import React, { use } from 'react';
+import StorySubjectInput from './_components/StorySubjectInput';
+
+export interface fieldData {
+  fieldName: string;
+  fieldValue: string;
+}
 const CreateStory = () => {
+  const onHandleUserSelection = (data: fieldData) => {
+    console.log(data);
+  };
+
   return (
     <div className='p-10 md:px-20 lg:px-40'>
       <h2 className='font-extrabold text-[70px] text-primary text-center'>
@@ -10,6 +21,16 @@ const CreateStory = () => {
         Unlock your creativity with AI: Craft stories like never before!Let our
         AI bring your imagination to life, one story at a time.
       </p>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-14'>
+        {/* Story Subject */}
+        <StorySubjectInput userSelection={onHandleUserSelection} />
+        {/* Story Type */}
+
+        {/* Age Group */}
+
+        {/* Image Style */}
+      </div>
     </div>
   );
 };
