fdb790f7ceac3c42d364e6c4d65c6d7272ac5bb8
krasi <krasimir.xristov@gmail.com>
2025-02-01 11:58:25 +0100

    Create hero for the create strory section

--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const CreateStory = () => {
-  return <div>CreateStory</div>;
+  return (
+    <div className='p-10 md:px-20 lg:px-40'>
+      <h2 className='font-extrabold text-[70px] text-primary text-center'>
+        CREATE YOUR STORY
+      </h2>
+      <p className='text-2xl text-primary text-center'>
+        Unlock your creativity with AI: Craft stories like never before!Let our
+        AI bring your imagination to life, one story at a time.
+      </p>
+    </div>
+  );
 };
 
 export default CreateStory;
