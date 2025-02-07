0874e66b782a634f07d948e397d8e6826d5bda49
krasi <krasimir.xristov@gmail.com>
2025-02-07 23:47:58 +0100

    add PostCSS configuration and update Tailwind CSS settings for improved styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      borderRadius: {
+        '2xl': '1rem',
+      },
+      boxShadow: {
+        soft: '0px 4px 6px rgba(0, 0, 0, 0.1)',
       },
     },
   },
