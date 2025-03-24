b68ada50c54ceab6a33815eda5ee5d3543c6f2bd
krasi <krasimir.xristov@gmail.com>
2025-03-24 11:07:47 +0100

    Refactor RegisterPage layout for improved responsiveness; update width constraints for better user experience

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 export default function RegisterPage() {
   return (
-    <div className='container flex h-screen w-screen flex-col items-center justify-center'>
-      <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+    <div className='container flex h-screen w-full flex-col items-center justify-center'>
+      <div className='mx-auto flex w-full max-w-[350px] flex-col justify-center space-y-6'>
         <div className='flex flex-col space-y-2 text-center'>
           <div className='mx-auto flex items-center justify-center'>
             <BookOpen className='h-8 w-8 text-primary' />
